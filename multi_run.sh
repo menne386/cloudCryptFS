f2a371747630ff1860c4cd29b1c8e60f3752664c
Menne Kamminga <kamminga.m@gmail.com>
2020-03-22 12:16:19 +0100

    Added possibility to specify how many test you want to run.

--- multi_run.sh
+++ multi_run.sh
@@ -3,7 +3,17 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-for VARIABLE in 1 2 3 4 5 6 7 8 9
+NUM=10
+
+if [[ "$1" == "-n" ]]; then
+	shift
+	NUM=$1
+	shift
+fi
+
+
+
+for (( VARIABLE=1; VARIABLE<=NUM; VARIABLE++ ))
 do
 ./fstest_docker.sh $@ 
 cp "test/log.txt" "test/run$VARIABLE.txt"
