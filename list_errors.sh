c3e65c87b97b4f24e6616e3c2f088939bcfbdf69
Menne Kamminga <kamminga.m@gmail.com>
2020-03-27 16:26:47 +0100

    -H to show file names

--- list_errors.sh
+++ list_errors.sh
@@ -4,8 +4,8 @@
 # license that can be found in the LICENSE file.
 
 echo "Passed:"
-grep "PASS" test/run*/log.txt
+grep -H "PASS" test/run*/log.txt
 echo "Warnings & Errors & Fails:"
-grep "WARNING\|ERROR\|FAIL" test/run*/log.txt
+grep -H "WARNING\|ERROR\|FAIL" test/run*/log.txt
 echo "Coredumps:"
 ls test/run*/core* -lha 2>/dev/null
